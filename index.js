c72a2386a8f3b86853a60b14f8dffc4b5076fae3
Nick Tang <tangnick@live.com>
2020-03-17 19:44:05 -0400

    socketio example for emitting and listening events

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ app.use(express.static(`${__dirname}/public`));
 
 server.listen(port, () => console.log(`listening on port ${port}`));
 
-io.sockets.on('connection', (socket) => {
-    console.log(`${socket.id} connected`);
-})
+let onlineClients = new Set();
+let nextVisitorNumber = 1;
+
+const onNewWebSocketConnection = (socket) => {
+    console.info(`Socket ${socket.id} has connected.`);
+    onlineClients.add(socket.id);
+
+    socket.on("disconnect", () => {
+        onlineClients.delete(socket.id);
+        console.info(`Socket ${socket.id} has disconnected.`);
+    });
+
+    socket.on("hello", helloMsg => console.info(`Socket ${socket.id} says: ${helloMsg}`));
+
+    socket.emit("welcome", `Welcome you are visitor number ${nextVisitorNumber++}`);
+};
+
+io.sockets.on('connection', onNewWebSocketConnection);
+
+// timed event
+let secondsSinceServerStarted = 0;
+setInterval(() => {
+    secondsSinceServerStarted++;
+    io.emit("seconds", secondsSinceServerStarted);
+    io.emit("online", onlineClients.size);
+}, 1000);
