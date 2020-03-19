d8992bf530abb4cfc57b2436df93369dbf9a093b
Nick Tang <tangnick@live.com>
2020-03-19 11:43:30 -0400

    add switching between templates for lobby, create game, join game screens

--- public/index.js
+++ public/index.js
@@ -19,6 +19,8 @@ const App = {
 
         App.gameArea = $('#gameArea');
         App.templateIntroScreen = $('#intro-screen-template').html();
+        App.templateWaitingScreen = $('#waiting-screen-template').html();
+        App.templateJoinScreen = $('#join-screen-template').html();
     },
 
     showInitScreen: () => {
@@ -28,17 +30,54 @@ const App = {
     bindEvents: () => {
         App.doc.on('click', '#btnCreateGame', App.createGame);
         App.doc.on('click', '#btnJoinGame', App.joinGame);
+        App.doc.on('click', '#btnBackIntro', App.showInitScreen);
+        App.doc.on('click', '#btnJoinRoom', App.joinGameRoom);
     },
 
     // game logic
     createGame: () => {
-        // todo check for name input
-        console.log("you clicked create game")
+        const username = $('#usernameInput').val();
+        console.log(username);
+        if(username) {
+            console.log("you clicked create game");
+            // need to emit some socket event so the backend can create a random gameId and join the gameId with socket.join
+            App.displayWaitingRoom();
+            return;
+        }
+        window.alert("You forgot your name");
     },
 
+    // go to join game screen
     joinGame: () => {
-        console.log("you clicked join game")
-    }
+        const username = $('#usernameInput').val();
+        console.log(username);
+        if(username) {
+            console.log("you clicked join game");
+            App.displayJoinRoom();
+            return;
+        }
+        window.alert("You forgot your name");
+    },
+
+    // actually join a game
+    joinGameRoom: () => {
+        const roomCode = $('#roomCodeInput').val();
+        if(roomCode) {
+            console.log("You joined room "+ roomCode);
+            // emit some socket event so the backend can add user to game room
+            App.displayWaitingRoom();
+            return;
+        }
+        window.alert("Room codes are 5 digits long, can you count?");
+    },
+
+    displayWaitingRoom: () => {
+        App.gameArea.html(App.templateWaitingScreen);
+    },
+
+    displayJoinRoom: () => {
+        App.gameArea.html(App.templateJoinScreen);
+    },
 };
 
 const main = () => {
