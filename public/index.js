f9546e5edfa0105f88242cef485edc9d472d3113
Nick Tang <tangnick@live.com>
2020-03-19 10:12:26 -0400

    breakstuff out into namespaces

--- public/index.js
+++ public/index.js
@@ -1,23 +1,38 @@
+// namespace for code relating to socket.io
+const IO = {
+    init: () => {
+
+    },
+};
+
+// namespace for code relating to the game logic
+const App = {
+
+    init: () => {
+        App.cacheElements();
+        App.showInitScreen();
+    },
+
+    cacheElements: () => {
+        App.gameArea = $('#gameArea');
+    },
+
+    showInitScreen: () => {
+        $('#gameArea').html()
+        const templateIntroScreen = $('#intro-screen-template').html();
+        App.gameArea.html(templateIntroScreen);
+    },
+};
+
 const main = () => {
     const socket = io();
     socket.on("connect", () => socket.emit("hello", `hello I am here`));
 
     console.log("jquery");
-    const gameArea = $('#gameArea');
-    const templateIntroScreen = $('#intro-screen-template').html();
-    console.log(templateIntroScreen);
 
-    gameArea.html(templateIntroScreen);
+    // initialize
+    App.init();
 
-
-    // const secondsElement = document.getElementById("seconds");
-    // socket.on("seconds", (seconds) => secondsElement.innerText = seconds.toString());
-
-    // const welcomeElement = document.getElementById("welcome");
-    // socket.on("online", online => onlineElement.innerText = online.toString());
-
-    // const onlineElement = document.getElementById("online");
-    // socket.on("welcome", welcomeMessage => welcomeElement.innerText = welcomeMessage);
-}
+};
 
 main();
