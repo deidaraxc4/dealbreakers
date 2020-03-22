4a313642211df077b4bba16c0ce7940119838ab3
Nick Tang <tangnick@live.com>
2020-03-22 18:33:19 -0400

    added over 200 red/white cards

--- cards.js
+++ cards.js
@@ -1,9 +1,53 @@
 const whiteCards = [
-    "rich", "handsome", "has big house", "is doctor", "is celebrity", "is tall", "is cool", "is nice", "is alive", "has car", "is funny"
+    "Is an astronaut", "Thinks you're the funniest person ever", "They're the president", "Is a CEO", "Gives great head",
+    "Is wholesome", "Plays guitar", "Reliable", "Genuine", "Patient", "Famous rapper", "D1 football player", "Challenger in League",
+    "Has a yacht", "Same religious views as you", "Childhood friend", "Will cook for you", "Is a Instagram influencer", "Is a heart surgeon",
+    "Loves kids", "Likes video games", "Famous Youtuber", "Is your long time crush", "Is the kinkiest person ever", "Is active",
+    "Likes to read", "Brings the best out of you", "Can bake", "Owns a private island", "You parents approve of them", "Values family",
+    "Adventurous", "Is hilarious", "Thinks you're the most beautiful person in the world", "Big dick energy", "Owns a farm",
+    "Is a sex transformer in bed", "Does anal", "Will do your taxes", "Is a celebrity", "Is Kpop star", "Calls you Oppa", "Into anime and chill",
+    "Cosplays", "Has your ideal body proportions", "Life of the party", "Is your waifu", "Has the cure for COVID-19", "Is your favorite pornstar",
+    "Owns a museum", "Sushi chef", "Anime voice actor", "Volunteers at animal shelters", "Takes the best photos for your Instagram",
+    "Owns every video game", "Will never judge you", "Has baby driver driving skills", "Only ever gets green lights when driving with them",
+    "Is a magician", "Very trusting", "Can play any anime opening on the piano", "Is a time traveler", "Has a dragon", "Always knows what to say",
+    "Smooth talker", "Hosts orgy parties", "Excellent dancer", "Is a genius", "195 IQ", "Owns an Iron Man suit", "Personal trainer",
+    "Good listener", "Is a genie", "Can fly", "Poops out money", "Has a deathnote", "Great handjobs", "Olympic athelete", "Amazing first date",
+    "Pilot", "Forever 21, physically", "Top 10 Twitch streamer", "NASCAR driver", "Lifts with you", "Likes rough sex", "Cannot get an STD",
+    "Scientist", "Gives you the greatest orgasms", "Mechanic", "Interior Designer", "Gives great stock investing advice", 
+    "Will retire by 40", "Soft skin", "Makes great boba", "Prepared for zombie apocalypse", "Reached Nirvana", "Actual Nigerian prince/princess",
+    "Philosophical", "Loyal", "Great sense of humor", "Respectful", "Honest", "Your teacher you had a crush on", "Your love in a past life",
+    "Emotionally available always", "Model", "Very affectionate", "Voice of an angel", "Radio show host", "Great back massages", "Humble",
+    "Has a twin that is down for threesomes", "Never an awkward moment", "Remembers the small things", "You're their first",
+    "Good at origami", "Master swordsman", "Can fix any tech problem", "Sends funny memes", "Great with children", "Will take a bullet for you",
+    "Excellent memory"
 ];
 
 const redCards = [
-    "ugly", "bad breath", "smelly", "cant read", "is dumb", "will cheat"
+    "They're your uncle", "Has the opposite sexuality as you", "Won't have sex before marriage", "Is on death row", "Is a flat earther",
+    "Is anti-vaxxer", "Calls you by their exes name all the time", "Has a deeper voice than you", "They're 17", "Is still in 5th grade",
+    "Has 15 felonies", "Always cuts you off when talking", "Is a baby", "Still sleeps with their mom", "Has an erection all the time",
+    "Will always leave you on read", "Is a cam girl", "Will cheat on you", "Always has a runny nose", "Will gamble your life savings",
+    "Can only climax while watching The Office", "Is forever unemployed", "Will always try to fight the waiter", "Always flirts with others",
+    "Worst smelling person in the world", "Is illiterate", "Terrible with money", "Still swiping on Tinder", "Plotting to kill you",
+    "Constantly laughing during sex", "Punches every kid they see", "Has cancer", "Has COVID-19", "Is sexually attracted to your mom",
+    "Is way too cocky", "Always messes up taking pictures of you", "Has to be fed by someone else", "Constantly farts", "Needs to masturbate hourly",
+    "Low sex drive", "Will never pay for the date", "Is long distance", "Has literal skeletons in their closet", "They're your best friend's ex",
+    "Loves kids, but not in the good way", "Is manipulative", "All your friends dislike her/him", "Has a drinking problem", "Is always high",
+    "Monotone voice", "Can only communicate thru singing", "Wears adult diapers", "Voted Trump", "Talks in a baby voice 24/7",
+    "Is twice their weight in their pictures", "Has 10 kids", "FBI's most wanted", "Always watching hentai", "Is your sister",
+    "Turns into a werewolf once a month", "Is insecure", "Always thinks you're cheating on them", "Only with you for the money",
+    "Is a slave owner", "Racist", "Looks like your dad", "Never trims/shaves their pubic area", "Constant garbage breath", 
+    "Boring sex", "Sex only happens once a year", "Has every STD", "Is a prostitute", "Their family hates you", "Likes to cuck you",
+    "Doesn't believe in using protection", "Erectile dysfunction", "Is a dog", "Shits on the floor", "Craps their pants everytime kissing you",
+    "Puts air quotes around every word they say", "Has no ambition", "Cries during sex", "Pedophile", "Can only talk while yelling",
+    "Has cactus tongue", "Has dicks on their back like a stegosaurus", "Constantly reminds you their ex was better in bed",
+    "Spends half your paycheck, every paycheck", "Neckbeard", "Biggest simp ever", "Has no physical assets whatsoever", "Kink shames you",
+    "Recently had sex with your parents", "Only has sex if they are getting gangbanged", "Has as much acne as they have hair", "Bald",
+    "Constantly recites the bee movie", "Thinks your job is lame", "Their kiss tastes like semen", "Likes having sex with homeless people",
+    "Speaks only in sarcasm", "Dead", "Never showers/baths", "Toxic", "Chain smokes 10 packs a day", "Hitler reincarnated", "Bites during oral",
+    "Shits during anal", "Obese", "Maturity of 5 year old", "Forgets your name constantly", "Shows no physicaly affection",
+    "Imaginary", "Hands are always ice cold", "Breaks your new phones", "Blind", "Unable to finish a sentence", "Height of a dwarf",
+    "Allergic to everything"
 ];
 
 module.exports = {
